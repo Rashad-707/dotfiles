f815a18548df30face753ae1c59e435630722392
RaShaD <61148691+Rashad-707@users.noreply.github.com>
2021-10-11 17:02:11 +0300

    Add files via upload

---
+++ .config/polybar/theme-1/launch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Terminate already running bar instances
+    killall -q polybar
+
+# Wait until the processes have been shut down
+    while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch Polybar, using default config location ~/.config/polybar/config
+polybar bar-right&
+polybar bar-right-background&
+polybar bar-left&
+polybar bar-left-background&
+polybar bar-center-background&
